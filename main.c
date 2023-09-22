f6f8bb9592dcbdd94138bda19b7bbd3143675703
Irenimoyan2 <122874674+Irenimoyan2@users.noreply.github.com>
2023-09-22 02:39:45 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,35 @@
+#include "monty.h"
+globs global = {0, NULL, NULL, NULL};
+
+/**
+ * main - Interpreter for monty bytecodes.
+ * @ac: argument count to program.
+ * @av: arguments to program.
+ * Return: 0 always.
+ */
+int main(int ac, char **av)
+{
+	size_t len = 0;
+	int line_num = 1;
+
+	if (ac != 2)
+	{
+		dprintf(2, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	global.fp = fopen(av[1], "r");
+	if (!global.fp)
+	{
+		dprintf(2, "Error: Can't open file %s\n", av[1]);
+		exit(EXIT_FAILURE);
+	}
+	while (getline(&(global.line), &len, global.fp) != -1)
+	{
+		execute_op(global.line, line_num);
+		line_num++;
+	}
+	fclose(global.fp);
+	free_stack(global.stack);
+	free(global.line);
+	return (0);
+}
